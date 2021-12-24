228522b84e45d17f2d02941d8e2244d9908bb370
최유민 <ytooom019@naver.com>
2021-12-24 13:32:53 +0000

    HTML,  CSS, JS arrowbtn

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 
 // Make navbar transparent on the top
 // Make homeContainer slowly fade down as the window scrolls down
+// Make arrowbtn show when as the window scrolls down
 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 const homeContainer = document.querySelector('.home__container');
 const homeContainerHeight = homeContainer.getBoundingClientRect().height;
+const arrowbtn = document.querySelector('.arrow__btn');
 
 document.addEventListener('scroll', () => {
   if(window.scrollY > navbarHeight)
@@ -17,29 +19,39 @@ document.addEventListener('scroll', () => {
     navbar.style.backgroundColor = 'transparent';
   
     homeContainer.style.opacity = 1 - window.scrollY/homeContainerHeight;
-
+    
+  if(window.scrollY > 300)
+    arrowbtn.style.transform = 'none';
+  else
+    arrowbtn.style.transform = 'translatey(100px)';
+  
 });
 
 
-// Make navbar item point each section
+// Make navbar item, contactbtn, arrowbtn point each section
+
 
 const navbarmenu = document.querySelector('.navbar__menu');
 const contactbtn = document.querySelector('#home button');
 
 navbarmenu.addEventListener('click', () => {
-  const target = event.target;
-  scrollIntoView(target);
+  scrollIntoView(event.target);
 });
 
 contactbtn.addEventListener('click', () => {
-  const target = event.target;
-  scrollIntoView(target);
+  scrollIntoView(event.target);
 });
+
+arrowbtn.addEventListener('click', () => {
+  scrollIntoView(event.target);
+});
+
+
+
 
 function scrollIntoView (selector) {
   
   const link = selector.dataset.link;
-  console.log(link);
   
   if (link == null){
     return;
